ea76a8aadb7de2f10e9ecb022ba0894d7041bd6f
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2023-11-06 16:31:38 -0300

    up dockerfile

--- payments/src/main/java/com/example/payments/payment/controller/PaymentController.java
+++ payments/src/main/java/com/example/payments/payment/controller/PaymentController.java
@@ -1,13 +1,14 @@
 package com.example.payments.payment.controller;
 
-import com.example.payments.payment.model.Payment;
-import com.example.payments.payment.service.PaymentService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/payments")
+@RequestMapping("/payments")
+@RestController()
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
