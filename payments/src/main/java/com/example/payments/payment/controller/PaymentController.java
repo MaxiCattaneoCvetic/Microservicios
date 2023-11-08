f11f30b842fd8b6805bd00931e57cc20ecfc3e9b
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2023-11-07 22:32:12 -0300

    adding dockercompose

--- payments/src/main/java/com/example/payments/payment/controller/PaymentController.java
+++ payments/src/main/java/com/example/payments/payment/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package com.example.payments.payment.controller;
 
 
+import com.example.payments.payment.model.Payment;
+import com.example.payments.payment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
